0df1db1c20a40f41d227f27e06a5595f7aee9cb9
Marcio <78marcio@gmail.com>
2023-10-05 17:35:16 +0200

    fix select fuels select

--- js/populateFuels.js
+++ js/populateFuels.js
@@ -4,7 +4,7 @@ function populateFuel() {
     let content = '';
   
     REF.fuelList.forEach(fuel => {     
-        const isActive = fuel == REF.product ? 'active' : '';
+        const isActive = fuel == REF.fuel ? 'active' : '';
       content += `
         <a role="menuitem" class="dropdown-item d-flex justify-content-between align-items-center ${isActive}" href="#" data-fuel="${fuel}" data-bs-toggle="button" aria-pressed="true">
           <span>${languageNameSpace.labels[fuel]}</span>
@@ -23,20 +23,18 @@ function populateFuel() {
       dropdownMenu.on('click', '.dropdown-item', function() {
         const target = $(this);
         const checkIcon = target.find('.fas.fa-check');
-      
+        
         dropdownMenu.find('.dropdown-item').removeClass('active');
         dropdownMenu.find('.fas.fa-check').addClass('invisible');
-      
+        
         target.addClass('active');
         checkIcon.removeClass('invisible');
-
         const selectedText = target.find('span').text();
         $('#selectFuel').text(selectedText).append('<i class="fas fa-caret-down"></i>');
-
-        REF.product = target.attr('data-fuel')  
+        REF.fuel = target.attr('data-fuel')  
         
-        populateConsumption()
-        enprices()
+        REF.full = 1;
+        tableData();
 
       });
   
