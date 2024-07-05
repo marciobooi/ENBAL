7ac79665523aebe43fc552d10dacf27c55914140
marcio <78marcio@gmail.com>
2024-07-05 15:07:31 +0200

    social test

--- js/social.js
+++ js/social.js
@@ -3,12 +3,9 @@ var socialNameSpace = {
   linkedIn: function () {
     var currentUrl = window.location.href;
     var encodedUrl = encodeURIComponent(currentUrl);
+    var description = encodeURIComponent("This visualization tool, created by Eurostat, displays an interactive energy balance table with a great level of detail.");
     var url =
-      "https://www.linkedin.com/shareArticle?mini=true&title=CompleteEnergyBalances&url=" +
-      encodedUrl;
-
-log(url)
-
+      "https://www.linkedin.com/shareArticle?mini=true&title=CompleteEnergyBalances&summary=" + description + "&url=" + encodedUrl;
     window.open(
       url,
       "",
@@ -20,7 +17,8 @@ log(url)
   twitter: function () {
     var currentUrl = window.location.href;
     var encodedUrl = encodeURIComponent(currentUrl);
-    var url = "https://twitter.com/share?text=CompleteEnergyBalances&url=" + encodedUrl;
+    var text = encodeURIComponent("This visualization tool, created by Eurostat, displays an interactive energy balance table with a great level of detail. CompleteEnergyBalances");
+    var url = "https://twitter.com/share?text=" + text + "&url=" + encodedUrl;
     window.open(
       url,
       "",
@@ -32,10 +30,9 @@ log(url)
   facebook: function () {
     var currentUrl = window.location.href;
     var encodedUrl = encodeURIComponent(currentUrl);
-    // console.log(currentUrl)
-    // console.log(encodedUrl)
+    var description = encodeURIComponent("This visualization tool, created by Eurostat, displays an interactive energy balance table with a great level of detail.");
     var url =
-      "https://www.facebook.com/sharer.php?t=CompleteEnergyBalances&u=" + encodedUrl;
+      "https://www.facebook.com/sharer.php?u=" + encodedUrl + "&quote=" + description;
     window.open(
       url,
       "",
@@ -45,75 +42,9 @@ log(url)
   },
 
   email: function () {
+    var subject = encodeURIComponent("Complete Energy Balances");
+    var body = encodeURIComponent("This visualization tool, created by Eurostat, displays an interactive energy balance table with a great level of detail. Check it out here: " + window.location.href);
     document.location =    
-      "mailto:ESTAT-ENERGY@ec.europa.eu?subject=Complete%20Energy%20Balances&body=" +
-      encodeURIComponent(window.location.href);
+      "mailto:ESTAT-ENERGY@ec.europa.eu?subject=" + subject + "&body=" + body;
   },
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-var socialNameSpace = {
-  //social media
-  linkedIn: function () {
-    var currentUrl = window.location.href;
-    var encodedUrl = encodeURIComponent(currentUrl);
-    var url =
-      "https://www.linkedin.com/shareArticle?mini=true&title=CompleteEnergyBalances&url=" +
-      encodedUrl;
-    window.open(
-      url,
-      "",
-      "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=450,width=650"
-    );
-    return false;
-  },
-
-  twitter: function () {
-    var currentUrl = window.location.href;
-    var encodedUrl = encodeURIComponent(currentUrl);
-    var url = "https://twitter.com/share?text=CompleteEnergyBalances&url=" + encodedUrl;
-    window.open(
-      url,
-      "",
-      "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=700"
-    );
-    return false;
-  },
-
-  facebook: function () {
-    var currentUrl = window.location.href;
-    var encodedUrl = encodeURIComponent(currentUrl);
-    // console.log(currentUrl)
-    // console.log(encodedUrl)
-    var url =
-      "https://www.facebook.com/sharer.php?t=CompleteEnergyBalances&u=" + encodedUrl;
-    window.open(
-      url,
-      "",
-      "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700"
-    );
-    return false;
-  },
-
-  email: function () {
-    document.location =    
-      "mailto:ESTAT-ENERGY@ec.europa.eu?subject=Complete%20Energy%20Balances&body=" +
-      encodeURIComponent(window.location.href);
-  },
-
-
-
 };
