3fb4a71e17ab3b2890083b50f5b068ed2b541739
marcio <78marcio@gmail.com>
2024-10-03 22:50:50 +0200

    fix spelling

--- js/social.js
+++ js/social.js
@@ -1,16 +1,19 @@
-
 var socialNameSpace = (function () {
   const text = {
-    EN: "This visualization tool, created by Eurostat, displays an interactive energy balance table with a great level of detail.",
+    EN: "This visualisation tool, created by Eurostat, displays an interactive energy balance table with a great level of detail.",
     FR: "Cet outil de visualisation, créé par Eurostat, affiche un tableau interactif de bilan énergétique avec un niveau de détail élevé.",
-    DE: "Dieses Visualisierungswerkzeug, erstellt von Eurostat, zeigt eine interaktive Energiebilanz-Tabelle mit einem hohen Detailgrad."
+    DE: "Dieses Visualisierungswerkzeug, erstellt von Eurostat, zeigt eine interaktive Energiebilanz-Tabelle mit einem hohen Detailgrad.",
   };
 
   const currentUrl = encodeURIComponent(window.location.href);
-  const language = (REF.language || 'EN').toUpperCase(); // Default to English and ensure uppercase
+  const language = (REF.language || "EN").toUpperCase(); // Default to English and ensure uppercase
 
   function openWindow(url, height = 450, width = 650) {
-    window.open(url, "", `menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=${height},width=${width}`);
+    window.open(
+      url,
+      "",
+      `menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=${height},width=${width}`
+    );
   }
 
   return {
@@ -36,8 +39,10 @@ var socialNameSpace = (function () {
     },
 
     email: function () {
-      const subject = encodeURIComponent("Energy prices");
-      const body = encodeURIComponent(`${text[language]} ${window.location.href}`);
+      const subject = encodeURIComponent("Energy balances");
+      const body = encodeURIComponent(
+        `${text[language]} ${window.location.href}`
+      );
       document.location = `mailto:ESTAT-ENERGY@ec.europa.eu?subject=${subject}&body=${body}`;
     },
   };
