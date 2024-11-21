72031b1107bb7d23532f6d43e0512c78c40ebf2b
marcio <78marcio@gmail.com>
2024-11-21 21:59:19 +0100

    ENBAL-19/20

--- js/domComponents/subNavBarComponent.js
+++ js/domComponents/subNavBarComponent.js
@@ -79,12 +79,35 @@ class SubNavbar {
           </ul>
         </div>
       </div>
-    
+
       <div id="chartOptionsMenu" class="d-none">
-        <div class="close-button-container">
-          <button id="closeChartMenuBtn" class="btn btn-primary close-chart-menu-btn" data-i18n-label="CLOSE">
-            <i class="fas fa-times" aria-hidden="true"></i>
-          </button>
+        <div class="close-button-container"> 
+        <button 
+        id="closeChartMenuBtn" 
+        class="ecl-button ecl-button--tertiary ecl-modal__close ecl-button--icon-only" 
+        type="button" 
+        data-i18n-label="CLOSE" 
+        aria-expanded="true" 
+        aria-controls="chartOptionsMenu">
+        <span class="ecl-button__container">
+          <!-- Visually hidden but accessible label -->
+          <span class="ecl-button__label sr-only" data-ecl-label="true">Close</span>
+          <!-- SVG Icon -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#0F1729"/>
+          </svg>    
+          </span>
+      </button>
+      
+
+
+
+
+
+
+
+
+
         </div>
         <div class="dropdown-grid">
           <div class="row w-75">
